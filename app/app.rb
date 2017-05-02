5fa95393aec46ab0288377254763451bcd8e5cd1
anaalta <suciuanamaria@yahoo.com>
2017-05-02 16:09:08 +0200

    feature test green for seeing links

--- app/app.rb
+++ app/app.rb
@@ -1,1 +1,12 @@
 require 'sinatra/base'
+require_relative './models/link'
+
+class Bookmark < Sinatra::Base
+
+get '/links' do
+  @link = Link.all
+  erb(:'links/index')
+
+end
+
+end
--- spec/features/see_list_of_links_spec.rb
+++ spec/features/see_list_of_links_spec.rb
@@ -4,6 +4,9 @@ feature 'viewing links' do
     Link.create(url: 'http://makersacademy.com', title: 'Makers Academy')
     visit '/links'
     expect(page.status_code).to eq 200
-    #expect(page).to have_content('Makers Academy')
+
+    within 'ul#links' do
+    expect(page).to have_content('Makers Academy')
+  end
   end
 end
